80c8227f1bc457b9c7dda323df50912f7a0e17e8
dean.jaquez <Dean.Jaquez@cffp.edu>
2018-02-12 08:33:01 -0700

    add stuff

---
+++ Builder/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Builder
+{
+    /// <summary>
+    /// MainApp startup class for Real-World 
+    /// Builder Design Pattern.
+    /// </summary>
+    public class MainApp
+
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        public static void Main()
+        {
+            VehicleBuilder builder;
+
+            // Create shop with vehicle builders
+            Shop shop = new Shop();
+
+            // Construct and display vehicles
+            builder = new ScooterBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new CarBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new MotorCycleBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Director' class
+    /// </summary>
+    class Shop
+
+    {
+        // Builder uses a complex series of steps
+
+        public void Construct(VehicleBuilder vehicleBuilder)
+        {
+            vehicleBuilder.BuildFrame();
+            vehicleBuilder.BuildEngine();
+            vehicleBuilder.BuildWheels();
+            vehicleBuilder.BuildDoors();
+        }
+    }
+
+    /// <summary>
+    /// The 'Builder' abstract class
+    /// </summary>
+    public abstract class VehicleBuilder
+
+    {
+        protected Vehicle vehicle;
+
+        // Gets vehicle instance
+        public Vehicle Vehicle{ get {return vehicle; } }
+
+        // Abstract build methods
+        public abstract void BuildFrame();
+        public abstract void BuildEngine();
+        public abstract void BuildWheels();
+        public abstract void BuildDoors();
+    }
+
+    /// <summary>
+    /// The 'Product' class
+    /// </summary>
+    public class Vehicle
+
+    {
+        private string _vehicleType;
+        private Dictionary<string, string> _parts =
+          new Dictionary<string, string>();
+
+        // Constructor
+        public Vehicle(string vehicleType)
+        {
+            this._vehicleType = vehicleType;
+        }
+
+        // Indexer
+        public string this[string key]
+        {
+            get { return _parts[key]; }
+            set { _parts[key] = value; }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("\n---------------------------");
+            Console.WriteLine("Vehicle Type: {0}", _vehicleType);
+            Console.WriteLine(" Frame : {0}", _parts["frame"]);
+            Console.WriteLine(" Engine : {0}", _parts["engine"]);
+            Console.WriteLine(" #Wheels: {0}", _parts["wheels"]);
+            Console.WriteLine(" #Doors : {0}", _parts["doors"]);
+        }
+    }
+}
---
+++ Builder/ScooterBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    /// <summary>
+    /// The 'ConcreteBuilder3' class
+    /// </summary>
+    public class ScooterBuilder : VehicleBuilder
+
+    {
+        public ScooterBuilder()
+        {
+            vehicle = new Vehicle("Scooter");
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "Scooter Frame";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "50 cc";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "2";
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "0";
+        }
+    }
+}
